023aee9fe1080f06de9fb815ff851db7b7d3dc23
HAOWEI\haowe <haowei.zhou>
2023-10-26 20:27:28 +0800

    w07-p3: add index.html as home page with links of class demo my github and my vercel

---
+++ javascript.js
@@ -0,0 +1,25 @@
+function showdemo(week){
+    const p = document.querySelector('.show-classdemo');
+    console.log('p', p);
+    switch(week){
+        case 2:
+            p.innerHTML = `<iframe src="./w02_61/index.html" width="100%" height="100%" />`
+            break;
+        case 3:
+            p.innerHTML = `<iframe src="./w03_61/index.html" width="100%" height="100%" />`
+            break;
+        case 4:
+            p.innerHTML = `<iframe src="./w04_61_blog/index.html" width="100%" height="100%" />`
+            break;
+        case 5:
+            p.innerHTML = `<iframe src="./w05_61/index.html" width="100%" height="100%" />`
+            break;
+        case 6:
+            p.innerHTML = `<iframe src="./w06_61/index.html" width="100%" height="100%" />`
+            break;
+        case 7:
+            p.innerHTML = `<iframe src="./index.html" width="100%" height="100%">`
+            break;
+                
+    }
+}
