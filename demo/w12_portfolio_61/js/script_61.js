e0849b6bb0020ac7dca7865ba9db14b265ab464c
haowei0218 <haoweizhou891@gmail.com>
2023-11-30 21:37:08 +0800

    W12-P4: Make nav-btn and close-btn works with navbar-fixed and show-sidebar

--- demo/w12_portfolio_61/js/script_61.js
+++ demo/w12_portfolio_61/js/script_61.js
@@ -6,10 +6,17 @@ const closeBtn = document.querySelector('#close-btn');
 
 closeBtn.addEventListener('click',function(){
   sidebar.classList.remove('show-sidebar');
+  if (window.scrollY > 80) {
+    navbar.classList.add('navbar-fixed');
+  } else {
+    navbar.classList.remove('navbar-fixed');
+  }
+
 })
 
 navBtn.addEventListener('click',function(){
   sidebar.classList.add('show-sidebar');
+  navbar.classList.remove('show-sidebar');
 })
 // add fixed class to navbar
 window.addEventListener('scroll', function () {
