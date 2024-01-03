334520acfa9cee3e93bc98056cd451e713f7ab5f
haowei212410061 <212410061@o365.tku.edu.tw>
2024-01-03 20:22:08 +0800

    click classdemo with js and html

--- 期末project/main.js
+++ 期末project/main.js
@@ -1,14 +1,24 @@
-function showClassDemo(classdemo) {
-    var elements = document.getElementsByClassName('.classdemo');
-    elements.classList.add('demo')
-}
-
-const black = document.getElementsByClassName('.fa-solid fa-toggle-on');
-black.addEventListener('click',function() {
-    if (this.checked) {
-        document.body.classList.add('dark');
-    }
-    else{
-        document.body.classList.remove('dark');
-    }
+//實現導覽列切換功能
+const navbars = document.querySelectorAll('.classdemo');
+const demos = document.querySelectorAll('.demo');
+navbars.forEach(function (navbar) {
+  navbar.addEventListener('click', function () {
+    demos.forEach(function (demo) {
+      demo.classList.toggle('demo');
+    });
+  });
 });
+
+const toggler = document.getElementById('theme-toggle');
+toggler.addEventListener('change', function () {
+  if (this.checked) {
+    document.body.classList.add('dark');
+  } else {
+    document.body.classList.remove('dark');
+  }
+});
+
+//點擊子導覽列 以iframe形式呈現在網頁上
+function showdemo(page) {
+  const PageContainer = document.getElementById('page-container');
+}
